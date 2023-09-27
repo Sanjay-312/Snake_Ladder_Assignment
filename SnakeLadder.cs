1acb7463b1368a3e70cdaaaf463eb542749017fb
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-27 12:24:24 +0530

    [sanjeeva] : initialized player one at 0 position

---
+++ SnakeLadder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderAssignment
+{
+    public class SnakeLadder
+    {
+        public void start()
+        {
+            int position = 0;
+            int player_one;
+            player_one = position;
+            Console.WriteLine("player one position is {0}",player_one);
+        }
+        public void Board()
+        {
+            start();
+
+        }
+    }
+}
