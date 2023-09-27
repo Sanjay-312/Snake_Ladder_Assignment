99171e89c152f6c69b4c8266dbff8a4c5d9568b1
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-27 12:32:11 +0530

    [sanjeeva] : rolled the dice generetd a random number

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -14,6 +14,16 @@ namespace SnakeLadderAssignment
             int player_one;
             player_one = position;
             Console.WriteLine("player one position is {0}",player_one);
+            player_one = roll_die();
+            Console.WriteLine("player one rolled the dice and got the position {0}",player_one);
+        }
+
+        public int roll_die()
+        {
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            return dice;
+
         }
         public void Board()
         {
